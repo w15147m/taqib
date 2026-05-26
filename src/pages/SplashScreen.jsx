66b601cd14a5c97191b3e18481461e6dee102420
waseem abbas <waseemofficee@gamil.com>
2026-05-26 23:49:04 +0500

    feat: replace splash screen raster images with SVG and update styling

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -4,10 +4,10 @@ import {
   Text, 
   Animated, 
   StatusBar, 
-  Image, 
-  ImageBackground,
   StyleSheet
 } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+import { taqeebatBgSvg } from '../assets/images/taqeebat_bg';
 
 const SplashScreen = ({ onFinish }) => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
@@ -38,44 +38,17 @@ const SplashScreen = ({ onFinish }) => {
     <View style={styles.container}>
       <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
       
-      <ImageBackground 
-        source={require('../assets/images/parts/main-bg.png')}
-        style={styles.backgroundImage}
-        resizeMode="cover"
+      <Animated.View 
+        style={{ 
+          opacity: fadeAnim, 
+          transform: [{ translateY: slideAnim }],
+          flex: 1,
+          width: '100%',
+          height: '100%',
+        }}
       >
-        <Animated.View 
-          style={{ 
-            opacity: fadeAnim, 
-            transform: [{ translateY: slideAnim }],
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'space-evenly',
-            paddingVertical: '12%',
-            paddingHorizontal: '8%'
-          }}
-        >
-          {/* Top Text Calligraphy */}
-          <Image 
-            source={require('../assets/images/parts/text_top.png')}
-            style={styles.textTop}
-            resizeMode="contain"
-          />
-
-          {/* Middle Calligraphy (Title) */}
-          <Image 
-            source={require('../assets/images/parts/text_middle.png.png')}
-            style={styles.textMiddle}
-            resizeMode="contain"
-          />
-
-          {/* Bottom Calligraphy (Supplication) */}
-          <Image 
-            source={require('../assets/images/parts/text_bottom.png')}
-            style={styles.textBottom}
-            resizeMode="contain"
-          />
-        </Animated.View>
-      </ImageBackground>
+        <SvgXml xml={taqeebatBgSvg} width="100%" height="100%" />
+      </Animated.View>
 
       {/* Subtle version indicator */}
       <View style={styles.versionContainer}>
@@ -89,26 +62,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#bce5ea', // matching light blue/teal background
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  },
-  textTop: {
-    width: '80%',
-    aspectRatio: 5.6,
-    maxHeight: 70,
-  },
-  textMiddle: {
-    width: '75%',
-    aspectRatio: 1.64,
-    maxHeight: 250,
-  },
-  textBottom: {
-    width: '70%',
-    aspectRatio: 1.67,
-    maxHeight: 180,
   },
   versionContainer: {
     position: 'absolute',

27be7f6277a0617a32033b320db607874c340e70
waseem abbas <waseemofficee@gamil.com>
2026-06-02 15:55:29 +0500

    chore: rename app module to taqeebat and update configuration source from app.json to package.json

--- android/app/src/main/java/com/reactnativetutorial/MainActivity.kt
+++ android/app/src/main/java/com/reactnativetutorial/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ReactNativeTutorial"
+  override fun getMainComponentName(): String = "taqeebat"
   override fun onCreate(savedInstanceState: Bundle?) {
         SplashScreen.show(this)
         super.onCreate(savedInstanceState)

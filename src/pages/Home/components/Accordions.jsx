b75ef14f6ac60cc541d857164183a09d76736349
waseem abbas <waseemofficee@gamil.com>
2026-06-07 20:58:14 +0500

    feat: add SearchAccordion component to enable real-time search across application content

--- src/pages/Home/components/Accordions.jsx
+++ src/pages/Home/components/Accordions.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
+import SearchAccordion from './components/SearchAccordion';
 import PrimaryAccordion from './components/PrimaryAccordion';
 import SecondaryAccordion from './components/SecondaryAccordion';
 
 const Accordions = ({handleLayout, handleOpen}) => {
   return (
     <View className="mt-4">
+      <SearchAccordion handleLayout={handleLayout} handleOpen={handleOpen} />
       <PrimaryAccordion handleLayout={handleLayout} handleOpen={handleOpen} />
       <SecondaryAccordion handleLayout={handleLayout} handleOpen={handleOpen} />
     </View>
